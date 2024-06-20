32332e5f681762cb0b3601c0c4f7309c86a511e5
Elkin Diaz <elkin.diaz@epn.du.ec>
2024-06-20 08:55:28 -0500

    Cambio3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,10 +12,21 @@ public class Main {
         //obj1.mostrar_datos();
         Ticket obj2 = new Ticket("Luisa", "1232343",0,4,"11/05/2021",true,1111,3);
         //obj2.mostrar_datos();
-        Servicio obj3 = new Servicio("Carlos","111233",0,3,"14/05/2222",true,1111,23,2,3);
+        Servicio obj3 = new Servicio("Carlos","111233",0,3,"14/05/2222",false,1111,23,2,3);
         //obj3.mostrar_datos();
-        Normal obj5 =new Normal("Marco","12123323",0,2,"4/11/2000",true,12,14,2,3,"Ventana");
-        obj5.mostrar_datos();
+        Pasajero pa2 = new Pasajero("Juan","11111");
+        //pa2.mostrar_datos();
+
+        Ticket p3 = new Ticket();
+        p3.setNombre("Elkin Diaz");
+        p3.setCedula("1727660902");
+        p3.setPrecio(0);
+        p3.setTipo_ruta(2);
+        p3.setFecha_ruta("21/05/2021");
+        p3.setRetorno(true);
+        p3.setN_ticket(1465);
+        p3.setN_asiento(20);
+        p3.mostrar_datos();
 
     }
 }
--- src/main/java/org/example/Viajes/Ruta.java
+++ src/main/java/org/example/Viajes/Ruta.java
@@ -16,6 +16,38 @@ public class Ruta extends Pasajero{
         this.retorno = retorno;
     }
 
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getTipo_ruta() {
+        return tipo_ruta;
+    }
+
+    public void setTipo_ruta(int tipo_ruta) {
+        this.tipo_ruta = tipo_ruta;
+    }
+
+    public String getFecha_ruta() {
+        return fecha_ruta;
+    }
+
+    public void setFecha_ruta(String fecha_ruta) {
+        this.fecha_ruta = fecha_ruta;
+    }
+
+    public boolean isRetorno() {
+        return retorno;
+    }
+
+    public void setRetorno(boolean retorno) {
+        this.retorno = retorno;
+    }
+
     //Metodo Personalizado
     public void precio_ruta(){
         if (tipo_ruta == 1 ){
