1bcc11548078b2a77c826f65b89dff0c34e4f2bb
1-21-01-E043-09\POO <santiago.cumbalepn.edu.ec>
2024-06-20 07:54:32 -0500

    Initial commit

---
+++ src/main/java/org/example/Viajes/Pasajero.java
@@ -0,0 +1,34 @@
+package org.example;
+
+public class Pasajero {
+    String nombre;
+    String cedula;
+
+    public Pasajero() {}
+    public Pasajero(String nombre, String cedula) {
+        this.nombre = nombre;
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    //Metodo
+    public void mostrar_datos(){
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Cedula: " + cedula);
+    }
+}
